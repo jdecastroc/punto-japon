71d47af6539a68f0cd2680ab0157f3da53a8ce6a
jdecastroc <burbujati@gmail.com>
2016-03-01 21:40:29 +0100

    Testing bash script for index files to elastic search

--- src/main/java/com/puntojapon/articles/ElasticSearch.java
+++ src/main/java/com/puntojapon/articles/ElasticSearch.java
@@ -1,19 +1,27 @@
 package com.puntojapon.articles;
 
-import java.io.File;
 import java.io.IOException;
 
 public class ElasticSearch {
 	
 	public static void filesIndexation() {
-		ProcessBuilder pb = new ProcessBuilder("src/main/resources/indexBlogs.sh");
-		pb.inheritIO();
-		pb.directory(new File("src/main/resources"));
-		try {
-			pb.start();
-		} catch (IOException e) {
+//		ProcessBuilder pb = new ProcessBuilder("src/main/resources/indexBlogs.sh");
+//		pb.inheritIO();
+//		pb.directory(new File("src/main/resources"));
+//		try {
+//			pb.start();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		try{
+			// Run command and wait till it's done
+	        Process p = Runtime.getRuntime().exec("src/main/resources/indexBlogs.sh");
+	        p.waitFor();
+		} catch (InterruptedException | IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			//e.printStackTrace();
+			System.out.println("Error while trying to execute indexBlogs.sh");
 		}
 	}
 
