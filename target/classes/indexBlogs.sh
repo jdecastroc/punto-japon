d48268371cd73f17a6b2c05f4f393ddc1c4bca32
jdecastroc <jdecastroc@gmail.com>
2016-03-01 21:27:17 +0100

    Fixed indexing files bug, continue with bash java call

--- target/classes/indexBlogs.sh
+++ target/classes/indexBlogs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 COUNTER=1
-for file in src/main/resources/articles/*.json; do
+for file in articles/*.json; do
 DATA=$(less $file)
-curl -XPUT 'http://51.255.202.84:9200/articulos/blogs/'$COUNTER -d "$DATA" -vn
-sleep 10
+curl -XPUT 'http://51.255.202.84:9200/blogs/articulos/'$COUNTER -d "$DATA" -vn
+sleep 4
 COUNTER=$(($COUNTER+1))
 done
+echo "finished"
