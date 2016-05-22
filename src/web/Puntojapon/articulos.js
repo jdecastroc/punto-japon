40aea3e733fc2ac64bea1b3a8f579a58fd30beab
jdecastroc <burbujati@gmail.com>
2016-05-22 20:50:47 +0200

    Made header for all the frontend pages. Responsive checked, test functionality.

--- src/web/Puntojapon/articulos.js
+++ src/web/Puntojapon/articulos.js
@@ -11,22 +11,6 @@ $(document).ready(function() {
         loadArticles(10);
     });
 
-    $('#moreArticles10').click(function() {
-        $('#show-data').fadeOut('slow', function() {
-            loadArticles(52);
-        });
-        $('#moreArticles10').hide();
-        $('#moreArticles100').show();
-    });
-
-    $('#moreArticles100').click(function() {
-        $('#show-data').fadeOut('slow', function() {
-            loadArticles(100);
-            $('#moreArticles100').hide();
-        });
-    });
-
-
     //Cargar articulos
     function loading() {
         //Animacion de carga
@@ -52,46 +36,42 @@ $(document).ready(function() {
 
                 if (status == "success") {
 
-                    console.log(data);
 
-                  //  showData.empty(); //Refresh the div where the articles are stored
+                    //  showData.empty(); //Refresh the div where the articles are stored
 
 
                     var jsonData = data.data; //parse json
                     var output = "";
-										if (articulosCargados < jsonData.length) {
-										console.log("Cargados: " + articulosCargados + "Total: " + jsonData.length);
-                    for (i = articulosCargados; i < showArticles; i++) {
-                        articulosCargados++;
-                        if (i % 2 == false) {
-                            output += '<div class="col_half">';
-                            output += '<h3>' + jsonData[i].Title + '</h3>';
-                            output += '<div class="testi-image pull-left img-responsive"><a href="#"><img src="img/articulos/' + jsonData[i].Author + '.jpg" alt="' + jsonData[i].Author + '"></a></div>';
+                    if (articulosCargados < jsonData.length) {
+                        for (i = articulosCargados; i < showArticles; i++) {
+                            articulosCargados++;
+                            if (i % 2 == false) {
+                                output += '<div class="col_half">';
+                                output += '<h3>' + jsonData[i].Title + '</h3>';
+                                output += '<div class="testi-image pull-left img-responsive"><a href="#"><img src="img/articulos/' + jsonData[i].Author + '.jpg" alt="' + jsonData[i].Author + '"></a></div>';
 
-                            output += ' <ul class="entry-meta clearfix"> <li><i class="icon-calendar3"></i>' + jsonData[i].PubDate + '</li> <li><a href="#"><i class="icon-user"></i>' + jsonData[i].Author + '</a></li></ul>';
-                            output += '<article><p> ' + jsonData[i].Description + '<p></article>';
-                            output += '<a class="btn btn-blog pull-right marginBottom10" href=" ' + jsonData[i].Link + '">Seguir leyendo</a>';
+                                output += ' <ul class="entry-meta clearfix"> <li><i class="icon-calendar3"></i>' + jsonData[i].PubDate + '</li> <li><a href="#"><i class="icon-user"></i>' + jsonData[i].Author + '</a></li></ul>';
+                                output += '<article><p> ' + jsonData[i].Description + '<p></article>';
+                                output += '<a class="btn btn-blog pull-right marginBottom10" href=" ' + jsonData[i].Link + '">Seguir leyendo</a>';
 
-                            //output+='<div class="divider"><i class="icon-circle"></i></div>';//Separador de artículos
-                            output += '</div>';
-                        } else {
-                            output += '<div class="col_half col_last">';
-                            output += '<h3>' + jsonData[i].Title + '</h3>';
-                            output += '<div class="testi-image pull-left img-responsive"><a href="#"><img src="img/articulos/' + jsonData[i].Author + '.jpg" alt="' + jsonData[i].Author + '"></a></div>';
+                                //output+='<div class="divider"><i class="icon-circle"></i></div>';//Separador de artículos
+                                output += '</div>';
+                            } else {
+                                output += '<div class="col_half col_last">';
+                                output += '<h3>' + jsonData[i].Title + '</h3>';
+                                output += '<div class="testi-image pull-left img-responsive"><a href="#"><img src="img/articulos/' + jsonData[i].Author + '.jpg" alt="' + jsonData[i].Author + '"></a></div>';
 
-                            output += ' <ul class="entry-meta clearfix"> <li><i class="icon-calendar3"></i>' + jsonData[i].PubDate + '</li> <li><a href="#"><i class="icon-user"></i>' + jsonData[i].Author + '</a></li></ul>';
-                            output += '<article><p> ' + jsonData[i].Description + '<p></article>';
-                            output += '<a class="btn btn-blog pull-right marginBottom10" href=" ' + jsonData[i].Link + '">Seguir leyendo</a>';
+                                output += ' <ul class="entry-meta clearfix"> <li><i class="icon-calendar3"></i>' + jsonData[i].PubDate + '</li> <li><a href="#"><i class="icon-user"></i>' + jsonData[i].Author + '</a></li></ul>';
+                                output += '<article><p> ' + jsonData[i].Description + '<p></article>';
+                                output += '<a class="btn btn-blog pull-right marginBottom10" href=" ' + jsonData[i].Link + '">Seguir leyendo</a>';
 
-                            //output+='<div class="divider"><i class="icon-circle"></i></div>';//Separador de artículos
-                            output += '</div>';
+                                //output+='<div class="divider"><i class="icon-circle"></i></div>';//Separador de artículos
+                                output += '</div>';
+                            }
                         }
-
-
+                        /*                    output += "</ul>";*/
+                        showData.append(output);
                     }
-/*                    output += "</ul>";*/
-                    showData.append(output);
-									}
 
                 }
             }
@@ -101,15 +81,12 @@ $(document).ready(function() {
     }
     //----------Articulos cargados----------
 
-    //header
     $('#header').hide();
 
     $(window).scroll(function() {
 
         var articleOffset = $("#show-data").offset().top;
         var articleOffsetBottom = $("#show-data").offset().top + $("#show-data").height();
-        console.log(articleOffsetBottom);
-
 
         if ($(this).scrollTop() > articleOffset) {
             $('#header').fadeIn();
@@ -117,19 +94,10 @@ $(document).ready(function() {
             $('#header').fadeOut();
         }
 
-        /*				if (($(this).scrollTop() + $(this).height() > articleOffsetBottom - 200) && ($(this).scrollTop() > articleOffset)) {
-        					console.log("Scroll bottom =" + $(this).scrollTop() + $(this).height());
-        					$('#show-data').fadeOut('slow', function() {
-        						console.log("He entrado");
-        							loadArticles(articulosCargados + 10);
-        					});
-        				}*/
-
-        if (!loading && $(window).scrollTop() >= ($('#carga').offset().top + $('#carga').outerHeight() - window.innerHeight)) {
+        if (!loading && $(this).scrollTop() >= ($('#carga').offset().top + $('#carga').outerHeight() - window.innerHeight - 100)) {
             loading = true;
-						loadArticles(articulosCargados + 10);
-						loading = false;
+            loadArticles(articulosCargados + 10);
+            loading = false;
         }
-
     });
 });
